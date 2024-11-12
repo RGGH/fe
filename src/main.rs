33c3f9ceaec4a104198b134d5791d044fcd615e5
RGGH <pisscompany@gmail.com>
2024-11-12 13:28:28 +0000

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use fastembed::{EmbeddingModel, InitOptions, TextEmbedding};
+use std::time::Instant;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Start timing
+    let start = Instant::now();
+
+    // With default InitOptions
+    let _model = TextEmbedding::try_new(Default::default())?;
+
+    // With custom InitOptions
+    let model = TextEmbedding::try_new(
+        InitOptions::new(EmbeddingModel::AllMiniLML6V2).with_show_download_progress(true),
+    )?;
+
+    let documents = vec![
+        "passage: Hello, World!",
+        "query: Hello, World!",
+        "passage: This is an example passage.",
+        // You can leave out the prefix but it's recommended
+        "fastembed-rs is licensed under Apache  2.0",
+    ];
+
+    // Generate embeddings with the default batch size, 256
+    let embeddings = model.embed(documents, None)?;
+
+    println!("Embeddings length: {}", embeddings.len()); // -> Embeddings length: 4
+    println!("Embedding dimension: {}", embeddings[0].len()); // -> Embedding dimension: 384
+    println!("{:?}", embeddings);
+
+    // Stop timing and calculate the duration
+    let duration = start.elapsed();
+
+    // Log the time taken
+    println!("Time taken: {:?}", duration); // approx 500ms
+
+    Ok(())
+}
